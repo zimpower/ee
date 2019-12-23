1bcc98002be08fb43b81c9e36e617a2aa76a7d63
Simon Hards <simon.hards@gmail.com>
2019-12-23 11:38:51 +0200

    set logger level to DEBUG

--- app/app.py
+++ app/app.py
@@ -32,7 +32,7 @@ formatter = logging.Formatter(
     '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.WARN)
+logger.setLevel(logging.DEBUG)
 
 
 def main():
